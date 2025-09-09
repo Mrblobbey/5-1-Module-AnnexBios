d42e1cc1afdda92c64c7c9a0853a6e745ecc10c7
basspies <haroldspies@hotmail.com>
2025-09-09 11:41:47 +0200

    header
    header en de footer gemaakt header bijna af maar nog een probleempje

---
+++ website/includes/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<header classs="header">
+        <div class="logo">
+        <img src=".png" alt="">
+        </div>
+    <nav class="nav">
+        <ul>
+            <a href="filmagenda.php">FILM AGENDA</a>
+            <a href="allevestingen.php">ALLE VESTINGEN</a>
+            <a href="contact.php">CONTACT</a>
+        </ul>
+    </nav>
+
+    <div class="nav2">
+        <label for="film">koop je tickets:</label>
+        <select name="" required>
+            <option value="" disabled selected>kies je film</option>
+            <select name="film" id="film">
+            <option value="minions">minions</option>
+            <option value="jasper">jasper</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+        </select>
+    </div>
+    <div class="">
+
+    </div>
+
+</header>
---
+++ website/index.php
@@ -0,0 +1,19 @@
+<?php
+include 'includes/header.php';
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include 'includes/footer.php';
+?>

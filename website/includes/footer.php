eb636440c665095f0e34726a1ed75e096ffc9795
jappie-p <glujasper@gmail.com>
2025-09-11 14:08:41 +0200

    footer

--- website/includes/footer.php
+++ website/includes/footer.php
@@ -1,25 +1,55 @@
-<div class="footer">
-<p> Lorem ipsum dolor sit amet, consectetuer<br> 
-    adipiscing elit. Aenean commodo ligula eget <br>
-    dolor. Aenean massa. Cum sociis natoque <br>
-    penatibus et magnis dis parturient montes,<br>
-    nascetur ridiculus mus. Donec quam felis,
-</p>
-<button class="button">
-LEES MEER
-</button>
-</div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+<footer class="main-footer">
+    <div class="footer-container">
+    <img src="Photos/footer_logo.svg" alt="">2
+        <div class="footer-content">
+            
+            <div class="footer-about">
+                <p> 
+                Lorem ipsum dolor sit amet, consectetuer<br> 
+                adipiscing elit. Aenean commodo ligula eget<br>
+                dolor. Aenean massa. Cum sociis natoque<br>
+                penatibus et magnis dis parturient montes,<br>
+                nascetur ridiculus mus. Donec quam felis.
+                </p>
+                <a class="footer-button" href="#">LEES MEER</a>
+            </div>
+
+            <div class="footer-nav">
+                <h3>NAVIGEER</h3>
+                <ul>
+                    <li><a href="#">Werken bij</a></li>
+                    <li><a href="#">Veelgestelde vragen</a></li>
+                    <li><a href="#">Vestegingen</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-social">
+                <h3>VOLG ONS</h3>
+                <div class="social-links">
+                    <a href="#" aria-label="Facebook"><i class="fab fa-facebook"></i></a>
+                    <a href="#" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+                    <a href="#" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                </div>
+            </div>
 
 
+            <div class="footer-decoration">
+                <img src="Photos/filmroll_footer.svg" alt="Film roll decoration">
+            </div>
+        </div>
+    </div>
 
-
-<footer class="footer2">
-    <p>&copy; 2024 Module Annex Bios</p>
+    <div class="footer-bottom">
+        <div class="footer-links">
+            <a href="#">Voorwaarden</a>
+            <a href="#">Privacy beleid</a>
+            <a href="#">Cookie disclaimer</a>
+        </div>
+    </div>
 </footer>
-
-
-
-
 
 </body>
 </html>

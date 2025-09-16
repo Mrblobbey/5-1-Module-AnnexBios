d070cc74a7be0563727a0a7c41e6b6a8e92e2312
Levi-Elias <le.sastro@outlook.com>
2025-09-16 12:32:48 +0200

    wachten op andere voor complete fix

--- website/stoelpagina/stoelpagina.php
+++ website/stoelpagina/stoelpagina.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo filemtime(filename:'css/style.css') ?>">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>annexbios</title>
 </head>
+<?php include '../includes/header.php'; ?>
+
 <body>
     
-
 <div class="background">
     <div class="koptekst">TICKETS BESTELLEN</div>
     <br><br>

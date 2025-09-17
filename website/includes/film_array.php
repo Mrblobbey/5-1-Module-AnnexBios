6f6c7355d30002415c18427effe86e003300f8d8
basspies <haroldspies@hotmail.com>
2025-09-17 16:10:46 +0200

    commit test

--- website/includes/film_array.php
+++ website/includes/film_array.php
@@ -1,18 +1,21 @@
 <?php
 $films = [
     [
+        'id' => '0',
         'titel' => 'Inception',
         'beschrijving' => 'Een dief die dromen steelt krijgt een laatste kans op verlossing. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk.De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk.',
         'tijd' => '19:30',
         'afbeelding' => 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg',
     ],
     [
+        'id' => '1',
         'titel' => 'The Lion King',
         'beschrijving' => 'Het verhaal van Simba, de leeuwenkoning. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk.De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk.De Avengers nemen het op tegen Thanos in een episch slotstuk.De Avengers nemen het op tegen Thanos in een episch slotstuk. ',
         'tijd' => '17:00',
         'afbeelding' => 'https://play-lh.googleusercontent.com/E4YJiRnNiYlM-PbjVrE2Zdr2d73SWbBTzarMIurgNNdr6c_Bh9IX05-ba6vdNR822EyG',
     ],
     [
+        'id' => '2',
         'titel' => 'Avengers: Endgame',
         'beschrijving' => 'De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk. De Avengers nemen het op tegen Thanos in een episch slotstuk.',
         'tijd' => '21:00',

cc2b3dcba01d551c712efe940fc5284b82b818cb
Mrblobbey <rody-100-@hotmail.com>
2025-09-17 14:31:08 +0200

    vandaag

---
+++ website/includes/config.php
@@ -0,0 +1,8 @@
+<?php
+require dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+
+// connectie met de api 
+$gluApiKey = 'https://u240066.gluwebsite.nl'; // de api bron 
+$gluApikey  = 'RzQPNjAVYzZRZspZsXrDjhsjFzG69KogrHB8Pkew'; // de key 
+?>

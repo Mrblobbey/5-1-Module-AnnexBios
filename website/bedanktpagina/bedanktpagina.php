01681290a4cfa99f7665ba7080d701aaacad5087
basspies <haroldspies@hotmail.com>
2025-09-26 09:58:37 +0200

    info niet doen

---
+++ website/bedanktpagina/bedanktpagina.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AnnexBios - Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="background"></div>
+        <?php
+        require_once '../includes/header.php';?>
+ 
+
+
+
+
+
+
+
+    <?php require_once '../includes/footer.php'; ?>
